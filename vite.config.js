53e70deaaf5e1fa4742916391b38634fa80f24eb
SamRandals <samnanezedu@gmail.com>
2025-10-15 19:17:56 -0500

    error fixeds

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/samPortfolio',
+  base:'/samPortfolio/',
   plugins: [react(), tailwindcss()],
 })
